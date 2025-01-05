eaf16455f8a1568831126825bad836b764027adc
Kurt M. Weber <weberk294@gmail.com>
2025-01-05 00:06:53 -0600

    platform.h, #define malloc

---
+++ include/platform.h
@@ -0,0 +1,18 @@
+/*
+ * Creator: Kurt M. Weber
+ * Created on: 2025-01-05
+ *
+ * This file is part of quanta, a component of the Cosmoverse.
+ * Licensed under Hippocratic License with clauses:
+ * HL3-CL-ECO-EXTR-FFD-MEDIA-MY-SUP-SV-TAL-USTA-XUAR
+ * See file LICENSE for full licensing informatin.
+ */
+
+#ifndef _QUANTA_PLATFORM_H
+#define _QUANTA_PLATFORM_H
+
+#ifdef ENV_HOSTED
+#define malloc malloc
+#endif
+
+#endif

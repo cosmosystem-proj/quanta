60625f65e0d6cb4fd74d5c1c3af633599d5bedb8
Kurt M. Weber <kurt.maxwell.weber@gmail.com>
2025-07-05 23:51:39 -0500

    added util.h with MAX() and MIN() macros

---
+++ include/util.h
@@ -0,0 +1,17 @@
+/*
+ * Creator: Kurt M. Weber
+ * Created on: 2025-07-05
+ *
+ * This file is part of quanta, a component of the Cosmoverse.
+ * Licensed under Hippocratic License with clauses:
+ * HL3-CL-ECO-EXTR-FFD-MEDIA-MY-SUP-SV-TAL-USTA-XUAR
+ * See file LICENSE for full licensing information.
+ */
+
+#ifndef _QUANTA_UTIL_H
+#define _QUANTA_UTIL_H
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+#endif
